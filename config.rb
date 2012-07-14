af71f76568bd65afbb1ccda90685e6cef67be643
Scott Kellum <scott@scottkellum.com>
2012-07-14 14:40:03 -0400

    CSS not working, moving it to root

--- config.rb
+++ config.rb
@@ -5,10 +5,10 @@ require 'singularitygs'
 
 # Set this to the root of your project when deployed:
 http_path = ''
-css_dir = '_/css'
-sass_dir = '_/sass'
-images_dir = '_/img'
-javascripts_dir = '_/js'
+css_dir = ''
+sass_dir = 'sass'
+images_dir = 'img'
+javascripts_dir = 'js'
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
