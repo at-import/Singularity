473c8c571454a6144dad851e0c5c45252ac362d3
Scott Kellum <scott@scottkellum.com>
2012-07-14 14:34:02 -0400

    new website

---
+++ config.rb
@@ -0,0 +1,18 @@
+# Require any additional compass plugins here.
+require 'compass'
+require 'singularitygs'
+# load '/Users/scottkellum/Sites/singularity/framework'
+
+# Set this to the root of your project when deployed:
+http_path = ''
+css_dir = '_/css'
+sass_dir = '_/sass'
+images_dir = '_/img'
+javascripts_dir = '_/js'
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+line_comments = false
+output_style = :compressed
+
+Sass::Script::Number.precision = 5
