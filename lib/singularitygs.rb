4b7a3ca2a4a4fd3adbc9cf5204969e50e1df2b19
Scott Kellum <scott@scottkellum.com>
2012-07-15 14:30:56 -0400

    Versioning way up.

--- lib/singularitygs.rb
+++ lib/singularitygs.rb
@@ -4,7 +4,7 @@ Compass::Frameworks.register("singularitygs", :path => "#{File.dirname(__FILE__)
 
 module CompassExtension
   
-  VERSION = "0.0.4"
+  VERSION = "0.0.7"
   DATE = "2012-06-23"
 
 end
