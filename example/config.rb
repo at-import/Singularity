8963e9cc02b7b896005326c92646a94a3138c993
Scott Kellum <scott@scottkellum.com>
2012-07-23 04:59:35 -0400

    Adding a few examples.

---
+++ example/config.rb
@@ -0,0 +1,8 @@
+# Require any additional compass plugins here.
+require 'compass'
+require 'singularitygs'
+
+# Set this to the root of your project when deployed:
+http_path = 'html'
+css_dir = 'css'
+sass_dir = 'sass'
