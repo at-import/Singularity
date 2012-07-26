118f8163335cae7b6075457f11d54e4c886e392f
Sam Richard <snugug@gmail.com>
2012-07-26 14:28:22 -0400

    Updated rb
    Until MS 1.0, we need to require Sassy math

--- lib/singularitygs.rb
+++ lib/singularitygs.rb
@@ -1,10 +1,12 @@
 require 'compass'
 require 'modular-scale'
+require 'sassy-math'
+
 Compass::Frameworks.register("singularitygs", :path => "#{File.dirname(__FILE__)}/..")
 
 module CompassExtension
   
-  VERSION = "0.0.7"
-  DATE = "2012-06-23"
+  VERSION = "0.0.8"
+  DATE = "2012-07-26"
 
 end
