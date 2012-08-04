0924c2782f3f4204bc2c5b8034fac58e7e33eaa0
Sam Richard <snugug@gmail.com>
2012-08-04 16:12:33 -0400

    Updated w/Breakpoint Require

--- lib/singularitygs.rb
+++ lib/singularitygs.rb
@@ -1,12 +1,13 @@
 require 'compass'
 require 'modular-scale'
 require 'sassy-math'
+require 'breakpoint'
 
 Compass::Frameworks.register("singularitygs", :path => "#{File.dirname(__FILE__)}/..")
 
 module CompassExtension
   
-  VERSION = "0.0.8"
-  DATE = "2012-07-26"
+  VERSION = "0.0.9"
+  DATE = "2012-08-04"
 
 end
