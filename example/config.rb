db773f12e8cc4761a6cdb159d3a7c83f24eb9ac2
Mason Wendell <mason@canarypromo.com>
2012-08-13 12:37:16 -0400

    fix test paths and write rtl test

--- example/config.rb
+++ example/config.rb
@@ -1,10 +1,11 @@
 # Require any additional compass plugins here.
-load '/Users/scottkellum/Sites/Singularity/framework'
+
 # require 'singularitygs'
 require 'modular-scale'
 require 'breakpoint'
 
 # Set this to the root of your project when deployed:
+add_import_path '../stylesheets'
 http_path = 'html'
 css_dir = 'css'
 sass_dir = 'sass'
