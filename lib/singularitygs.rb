12ccaa09185539359c005b7d7d8e5679ae28f081
Sam Richard <snugug@gmail.com>
2013-03-31 09:20:10 -0400

    Updated lib!

--- lib/singularitygs.rb
+++ lib/singularitygs.rb
@@ -5,6 +5,6 @@ require 'breakpoint'
 Compass::Frameworks.register("singularitygs", :path => "#{File.dirname(__FILE__)}/..")
 
 module SingularityGS
-  VERSION = "1.0.alpha.0"
-  DATE = "2012-11-06"
+  VERSION = "1.0.0"
+  DATE = "2013-03-30"
 end
