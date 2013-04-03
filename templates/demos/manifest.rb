f263e8dedab082922460daed3cfb695f3367243f
Sam Richard <snugug@gmail.com>
2013-04-03 15:04:09 -0400

    Demo Templates

---
+++ templates/demos/manifest.rb
@@ -0,0 +1,17 @@
+description "Singularity Demos"
+
+discover :stylesheets
+file 'isolation.html', :to => 'isolation.html'
+file 'float.html', :to => 'float.html'
+
+help %Q{
+  For help with Singularity, please ask a question on Stack Overflow (http://stackoverflow.com/questions/ask) tagged with "singularitygs".
+
+  To file an issue with Singularity, please use our GitHub Issue Queue (https://github.com/Team-Sass/Singularity/issues).
+}
+
+welcome_message %Q{
+  Welcome to the Singularity Demo!
+
+  These two sites, with their accompanying Sass, should give you a working example as to how Singularity works with its two out-of-the-box output styles.
+}
