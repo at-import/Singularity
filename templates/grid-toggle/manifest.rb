6276bf26c5cfd20edd68c60ebe2c755613be7b1e
Sam Richard <snugug@gmail.com>
2013-07-18 13:48:27 -0400

    Grid Toggle template

---
+++ templates/grid-toggle/manifest.rb
@@ -0,0 +1,22 @@
+description "Singularity Grid Toggle"
+
+discover :javascripts
+
+help %Q{
+  For help with Singularity, please ask a question on Stack Overflow (http://stackoverflow.com/questions/ask) tagged with "singularitygs".
+
+  To file an issue with Singularity, please use our GitHub Issue Queue (https://github.com/Team-Sass/Singularity/issues).
+}
+
+welcome_message %Q{
+  Welcome to Singularity! Grids Without Limits.
+
+  See our Getting Started guide at http://singularity.gs to get started and for full documentation.
+
+  If you need support for IE6/7, you should install the Border Box polyfill. Simply type `compass install singularitygs/box-sizing` in your command line from the root of your project folder (where config.rb is).
+
+
+  For help with Singularity, please ask a question on Stack Overflow (http://stackoverflow.com/questions/ask) tagged with "singularitygs".
+
+  To file an issue with Singularity, please use our GitHub Issue Queue (https://github.com/Team-Sass/Singularity/issues).
+}
