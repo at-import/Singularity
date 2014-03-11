abe72c6eeb61216ad44e1c847a64ed6c26e724fd
Sam Richard <snugug@gmail.com>
2014-03-11 15:40:25 -0400

    Proper checking to see if Compass is available, and add register the framework if it is

--- lib/singularitygs.rb
+++ lib/singularitygs.rb
@@ -1,24 +1,13 @@
-module SingularityGS
+require 'breakpoint'
 
+if (defined? Compass)
+  Compass::Frameworks.register(
+    "singularitygs",
+    :path => "#{File.dirname(__FILE__)}/.."
+  )
+end
+
+module SingularityGS
   VERSION = "1.2.0.rc.7"
   DATE = "2014-03-11"
-
-  class << self
-    def load!
-      require 'breakpoint'
-      register_compass_extension if compass?
-    end
-
-    def compass?
-      defined?(::Compass)
-    end
-
-    def register_compass_extension
-      ::Compass::Frameworks.register(
-        "singularitygs",
-        :path => "#{File.dirname(__FILE__)}/.."
-      )
-    end
-  end
-
 end
