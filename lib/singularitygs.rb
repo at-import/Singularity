e90170f06b56316cb9ae191b497ee6f7dc51ffe9
Sam Richard <snugug@gmail.com>
2014-07-28 10:13:22 -0400

    Made Singularity available to the Sass Path

--- lib/singularitygs.rb
+++ lib/singularitygs.rb
@@ -1,12 +1,16 @@
+singularitygs_path = File.expand_path('../stylesheets', __FILE__)
+
 if (defined? Compass)
   require 'breakpoint'
   Compass::Frameworks.register(
     "singularitygs",
-    :path => "#{File.dirname(__FILE__)}/.."
+    :stylesheets_directory => singularitygs_path
   )
+else
+  ENV["SASS_PATH"] = [ENV["SASS_PATH"], singularitygs_path].compact.join(File::PATH_SEPARATOR)
 end
 
 module SingularityGS
-  VERSION = "1.2.1"
-  DATE = "2014-05-03"
+  VERSION = "1.2.2"
+  DATE = "2014-07-24"
 end
