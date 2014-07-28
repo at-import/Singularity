1a64c4efb18891ffa6b8ef5493c35484131d0453
Sam Richard <snugug@gmail.com>
2014-07-28 10:33:06 -0400

    Path to make path work in Compass

--- lib/singularitygs.rb
+++ lib/singularitygs.rb
@@ -1,16 +1,17 @@
-singularitygs_path = File.expand_path('../stylesheets', __FILE__)
+singularitygs_sass_path = File.expand_path('../stylesheets', __FILE__)
+singularitygs_path = "#{File.dirname(__FILE__)}/.."
 
 if (defined? Compass)
   require 'breakpoint'
   Compass::Frameworks.register(
     "singularitygs",
-    :stylesheets_directory => singularitygs_path
+    :path => singularitygs_path
   )
 else
-  ENV["SASS_PATH"] = [ENV["SASS_PATH"], singularitygs_path].compact.join(File::PATH_SEPARATOR)
+  ENV["SASS_PATH"] = [ENV["SASS_PATH"], singularitygs_sass_path].compact.join(File::PATH_SEPARATOR)
 end
 
 module SingularityGS
-  VERSION = "1.2.2"
+  VERSION = "1.2.3"
   DATE = "2014-07-24"
 end
