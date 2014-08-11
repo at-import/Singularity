9c183057f0115e938fda77701d8531fc6e2576dd
Sam Richard <snugug@gmail.com>
2014-08-11 12:31:08 -0400

    Updated No-Compass Code to Breakpoint's
    And version bump

--- lib/singularitygs.rb
+++ lib/singularitygs.rb
@@ -1,17 +1,17 @@
-singularitygs_sass_path = File.expand_path('../stylesheets', __FILE__)
-singularitygs_path = "#{File.dirname(__FILE__)}/.."
+base_directory  = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+singularitygs_sass_path = File.join(base_directory, 'stylesheets')
 
 if (defined? Compass)
   require 'breakpoint'
   Compass::Frameworks.register(
     "singularitygs",
-    :path => singularitygs_path
+    :path => base_directory
   )
 else
   ENV["SASS_PATH"] = [ENV["SASS_PATH"], singularitygs_sass_path].compact.join(File::PATH_SEPARATOR)
 end
 
 module SingularityGS
-  VERSION = "1.2.3"
-  DATE = "2014-07-24"
+  VERSION = "1.3.0"
+  DATE = "2014-08-11"
 end
