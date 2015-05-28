1984020ffb396b1a600dac7ca876a36c58adb17a
Sam Richard <snugug@gmail.com>
2015-05-28 07:42:54 -0400

    New node test runner

--- tests/index.js
+++ tests/index.js
@@ -1,57 +1,58 @@
 var sass = require('node-sass'),
     jsdiff = require('diff'),
     fs = require('fs-extra'),
+    i = 0,
     glob = require('glob');
 
 glob('./tests/**/*.scss', function (err, files) {
   if (err) throw err;
 
-  console.log(files);
-
   files.forEach(function (file) {
     if (file.charAt(0) !== '_') {
       sass.render({
         file: file,
-        outputStyle: 'nested',
+        outputStyle: 'expanded',
         includePaths: [
           '../stylesheets/',
           '../vendor/ruby/2.0.0/gems/sassy-maps-0.4.0/sass/',
           '../vendor/ruby/2.0.0/gems/breakpoint-2.5.0/stylesheets/'
-        ],
-        success: function(result) {
-          // console.log(result.css);
-          fs.readFile('./controls/' + file.slice(8, -4) + 'css', function (ctrlErr, ctrlFile) {
-            if (ctrlFile) {
-              var results = result.css,
-                  diff;
+        ]
+      }, function(err, result) {
+          if (err) throw err;
 
-              // Libsass reverses quotes.
-              // results = results.replace(/'/g, '!single\!');
-              // results = results.replace(/"/g, '\'');
-              // results = results.replace(/\!single\!/g, '"');
-              // results += '\n';
+          // console.log(result);
+          var file = result.stats.entry.replace(process.cwd() + '/tests/', '');
+          file = file.replace('.scss', '.css');
 
-              diff = jsdiff.createPatch(file + '.patch', ctrlFile.toString(), results);
+          if (file.charAt(0) !== '_') {
+            fs.readFile('./controls/' + file, function (ctrlErr, ctrlFile) {
+              var diff = jsdiff.diffCss(ctrlFile.toString(), result.css.toString()),
+                  diffCount = 0;
 
-              // console.log(diff);
-              // console.log(Object.keys(diff));
-              fs.outputFile('./diff/' + file.slice(8, -4) + 'css.diff', diff, function (writeErr, writeFile) {
-                if (writeErr) throw writeErr;
+              diff.forEach(function(part){
+                // green for additions, red for deletions
+                // grey for common parts
+                var color = part.added ? 'green' :
+                  part.removed ? 'red' : 'grey';
+
+                if (color !== 'grey') {
+                  diffCount++;
+                }
               });
-              // console.log(diff);
-              // console.log(ctrlFile.toString() === result.css);
-              // diff.diffLines(ctrlFile.toString(), result.css, function (fileDiff) {
-                // console.log(fileDiff);
-                // console.log(cssDiff);
-                // fs.writeFile('./diff/' + file.slice(0, -4) + 'css.diff', cssDiff);
-              // });
-            }
-          });
-        },
-        error: function (error) {
-          throw error.message;
-        }
-      });
+
+              if (diffCount) {
+                console.log(file);
+                diff = jsdiff.createPatch(file, ctrlFile.toString(), result.css.toString());
+
+                fs.outputFile('./diff/' + file + '.diff', diff, function (writeErr, writePatch) {
+                  if (writeErr) throw writeErr;
+
+                  // throw 'Diff in output for ' + file;
+                });
+              }
+            });
+          }
+        });
     }
-  })
+  });
 });
